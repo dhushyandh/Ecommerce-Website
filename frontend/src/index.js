7d1aadab4f07f6ee294e3bff6f3c62b992eec6d2
Your Name <you@example.com>
2026-01-04 20:44:05 +0530

    chore(frontend): set REACT_APP_API_URL env and axios baseURL

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
+import axios from 'axios';
+
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || '';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

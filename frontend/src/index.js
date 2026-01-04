7df6b2112ad707b0b4268eca8f78c8ce90db1a53
Your Name <you@example.com>
2026-01-04 22:28:29 +0530

    chore: commit recent changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
-import axios from 'axios';
-
-
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://16.170.249.115:8000/api';
-axios.defaults.withCredentials = false;
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

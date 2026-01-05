33737efe530e319b12f6e5c090b55d3e51a751de
Your Name <you@example.com>
2026-01-05 18:00:02 +0530

    Configure for Render deployment

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_LOCAL_URI)
+    let DB_URI = "";
+    if(process.env.NODE_ENV === 'development') DB_URI = process.env.DB_LOCAL_URI;
+    if(process.env.NODE_ENV === 'production') DB_URI = process.env.DB_URI;
+
+    mongoose.connect(DB_URI)
     .then(con => {
         console.log(`MongooDB is Connected to the host: ${con.connection.host}`)
     })
